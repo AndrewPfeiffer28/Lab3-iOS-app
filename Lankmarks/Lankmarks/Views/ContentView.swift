61f0c169c706411d4807a63b2f83f1788378f01e
AndrewPfeiffer28 <141455250+AndrewPfeiffer28@users.noreply.github.com>
2023-11-08 21:42:58 -0600

    Add files via upload

---
+++ Lankmarks/Lankmarks/Views/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+
+
+struct ContentView: View {
+
+    var body: some View {
+
+        LandmarkList()
+
+    }
+
+}
+
+
+
+#Preview {
+
+    ContentView()
+
+        .environment(ModelData())
+
+}
---
+++ Lankmarks/Lankmarks/Views/Helper/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Lankmarks
+//
+//  Created by Andrew Pfeiffer on 10/19/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
